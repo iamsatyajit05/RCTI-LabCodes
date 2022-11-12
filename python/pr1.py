61997012e2d47588a572daa09843c8cb6a512248
Satyajit <thisissatyajit05@gmail.com>
2022-11-12 12:31:37 +0530

    Practical 1 Done

---
+++ python/pr1.py
@@ -0,0 +1,9 @@
+# Steps to download Python
+
+# 1. Go to python.org and download the executable installer
+# 2. Run the executable installer
+# 3. Check the "Installer Launcher..." & "Add Python 3.x to PATH" checkbox
+# 4. Select Customize Installation
+# 5. Check all the options and click NEXT
+# 6. Select the path where python should be downloaded and click on Install
+# 7. Congratulations, Python is Installed

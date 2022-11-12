70ec77784bbd814302f47efb8670f515685615f6
Satyajit <thisissatyajit05@gmail.com>
2022-11-12 12:31:56 +0530

    Practical 2 Done

---
+++ python/pr2.py
@@ -0,0 +1,14 @@
+# Write a program o read your name, contact number, email, and birthday, and print those details on the screen
+
+print("Hello, Please enter your details")
+
+name = input("Enter your name: ")
+contact_number = input("Enter your contact number: ")
+email = input("Enter your email: ")
+date_of_birth = input("Enter your date of birth: ")
+
+print("\nHere is your details")
+print("Name: ", name)
+print("Contact Number: ", contact_number)
+print("Email: ", email)
+print("Date of Birth: ", date_of_birth)

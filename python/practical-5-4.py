67faa1f26126dda8c2961832dee2597cef28985a
PIROshadow <114088441+PIROshadow@users.noreply.github.com>
2022-11-28 19:12:03 +0530

    Delete practical-5-4.py

--- python/practical-5-4.py
+++
@@ -1,8 +0,0 @@
-start = ord('!')
-end = ord('~')
-count = start
-while count <= end:
-    for i in range(5):
-        print(f"({chr(count)} = {count} = {hex(count)})")
-        count += 1
-    print()

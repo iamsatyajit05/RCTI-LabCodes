477d4f48d28d7830ac35f8577196c51c1a5fa257
Satyajit <thisissatyajit05@gmail.com>
2022-11-30 16:35:09 +0530

    Greatest Common Divisor Done

--- data_structure/practical2/pr7-3.c
+++ data_structure/practical2/pr7-3.c
@@ -1,1 +1,31 @@
 // Write a program to find Greatest Common Divisor using recursion.
+
+#include <stdio.h>
+
+int gcd(int a, int b){
+    if(a > b) {
+        return gcd(a-b, b);
+    }
+
+    else if(a < b)  {
+        return gcd(a, b-a);
+    }
+    
+    else {
+        return a;
+    }
+}
+
+int main() {
+    int num1, num2, result;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    result = gcd(num1, num2);
+
+    printf("\nGreatest Common Divisor of %d and %d is %d", num1, num2, result);
+}
